5b42d32ca052b45bb60ee2cf934484e5cfaf9c2a
hannah gaskins <hannahgaskins@hannahs-MacBook-Pro.local>
2016-06-27 14:22:41 -0700

    added code challenge

---
+++ hannahGaskins/week4CodeChallenges/codeChallengeDay16.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Hannah Gaskins 6.27.2016
+
+var testArray = [1, 44, 3, 10, 5, 4, 6]
+
+func returnMiddleThree(input: [Int]) -> [Int] {
+    
+    var outputArray: [Int] = []
+    
+    let median = (input.count - 1)/2
+    
+    outputArray.append(input[median - 1])
+    
+    outputArray.append(input[median])
+    
+    outputArray.append(input[median + 1])
+    
+    return outputArray
+    
+}
+
+returnMiddleThree(testArray)
+
+
